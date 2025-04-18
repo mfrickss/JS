66f13a782f59bb60a5f38d642fc1dd5522f29fe6
mfrickss <mfricks777@gmail.com>
2025-04-17 22:01:22 -0300

    mais sobre functions

--- Time2Code/4_Funções/script.js
+++ Time2Code/4_Funções/script.js
@@ -54,3 +54,77 @@ function sum(n1, n2){
 }
 
 console.log(`Sum = ${sum(d, e)}` );
+
+// 2 - function scope && nasted scopes
+
+let y = 15; // y fora da função pode ser de um valor diferente.
+
+function testandoEscopo(){
+    let y = 20;
+    console.log(`Y dentro da função é: ${y}`);
+}
+
+testandoEscopo();
+
+console.log(`Y fora da função é: ${y}`);
+
+testandoEscopo();
+
+let m = 10;
+
+function nastedScope(){  //Manteve a ordem de execução com os valores aninhados.
+    let m = 20;
+
+    if(true){
+        let m = 30;
+
+        if(true){
+            let m = 40;
+            console.log(m);
+        }
+
+        console.log(m);
+    }
+    console.log(m);
+}
+
+nastedScope();
+
+console.log(m);
+
+// 3 - Arrow function
+
+const testeArrow = () => {
+    console.log("Esta é uma arrow function");
+}
+
+testeArrow();
+
+const parOuImpar = (n) => {
+    if(n % 2 === 0){
+        console.log("Par");
+        return
+    }
+    console.log("Ímpar");
+}
+
+parOuImpar(5);
+
+parOuImpar(10);
+
+const raizQuadrada = (x) => {
+    return x * x
+}
+console.log(raizQuadrada(4));
+
+const raizQuadrada2 = (x) => x * x;
+
+console.log(raizQuadrada2(5));
+console.log(raizQuadrada2(12));
+
+const helloWorld = () => console.log("Hello world");
+
+helloWorld();
+
+console.log(helloWorld()); //=undefined
+
