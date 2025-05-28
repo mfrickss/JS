4a842d509aaeb3db3072ec0e9e4d17e73ea2f141
ricardo camargo <mfricks777@gmail.com>
2025-05-28 18:21:58 -0300

    prototype

--- Time2Code/6_poo_JS/script.js
+++ Time2Code/6_poo_JS/script.js
@@ -1,3 +1,4 @@
+// 1 - metódos
 const animal = {
   nome: "Marri",
   latir: function () {
@@ -28,3 +29,40 @@ console.log(pessoa.getNome());
 pessoa.setNome("Pedro");
 
 console.log(pessoa.getNome());
+
+// 3 - prototype
+
+const text = "asd";
+
+console.log(Object.getPrototypeOf(text));
+
+const bool = true;
+
+console.log(Object.getPrototypeOf(bool));
+
+const arr = [];
+
+console.log(arr.length);
+
+console.log(Object.getPrototypeOf(arr));
+console.log(Object.getPrototypeOf(arr) === Array.prototype);
+
+// 4 - mais sobre prototype
+
+const myObject = {
+  a: "Olá",
+};
+
+console.log(Object.getPrototypeOf(myObject));
+
+console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+const mySecondObject = Object.create(myObject);
+
+console.log(mySecondObject);
+
+console.log(mySecondObject.a);
+
+console.log(Object.getPrototypeOf(mySecondObject) === myObject);
+
+//
