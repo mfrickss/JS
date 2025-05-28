c6cdcd03bbdd3b540c055ba6cf69e730b774c1f4
ricardo camargo <mfricks777@gmail.com>
2025-05-28 18:05:07 -0300

    aprofundando em métodos

--- Time2Code/6_poo_JS/script.js
+++ Time2Code/6_poo_JS/script.js
@@ -8,3 +8,23 @@ const animal = {
 console.log(animal.nome);
 
 animal.latir();
+
+// 2 - aprofundando em métodos
+
+const pessoa = {
+  nome: "Ricks",
+  getNome: function () {
+    return this.nome;
+  },
+  setNome: function (novoNome) {
+    this.nome = novoNome;
+  },
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.getNome());
+
+pessoa.setNome("Pedro");
+
+console.log(pessoa.getNome());
